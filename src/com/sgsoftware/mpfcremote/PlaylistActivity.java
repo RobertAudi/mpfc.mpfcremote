ed96b3d59f37c70d637d50e610be93d1c22d0749
sg2 <sergey.e.galanov@gmail.com>
2011-09-25 19:34:10 +0400

    Support going upwards in directories.

--- src/com/sgsoftware/mpfcremote/PlaylistActivity.java
+++ src/com/sgsoftware/mpfcremote/PlaylistActivity.java
@@ -18,6 +18,7 @@ public class PlaylistActivity extends ListActivity
 	implements View.OnClickListener, AdapterView.OnItemClickListener {
 
 	String m_curDir;
+	boolean m_hasParent;
 	RemotePlayer.DirEntry[] m_entries;
 
 	@Override
@@ -45,11 +46,14 @@ public class PlaylistActivity extends ListActivity
 		}
 		case R.id.playlist_add: {
 			int count = getListView().getChildCount();
-			for ( int i = 0; i < count; i++ ) {
+			int i = 0, ei = 0;
+			if (m_hasParent)
+				i++;
+			for ( ; i < count; i++, ei++ ) {
 				CheckBox cb = (CheckBox)((ViewGroup)(getListView().getChildAt(i))).getChildAt(0);
 				boolean ch = cb.isChecked();
 				if (ch)
-					MainActivity.getPlayer().add(m_curDir + "/" + m_entries[i].name);
+					MainActivity.getPlayer().add(m_curDir + "/" + m_entries[ei].name);
 			}
 			break;
 		}
@@ -59,21 +63,39 @@ public class PlaylistActivity extends ListActivity
 
 	@Override
 	public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+		if (m_hasParent) {
+			if (position == 0) {
+				int l = m_curDir.length() - 1;
+				while (m_curDir.charAt(l) == '/')
+					l--;
+				l = m_curDir.lastIndexOf('/', l) + 1;
+				m_curDir = m_curDir.substring(0, l);
+				loadDir();
+				return;
+			}
+
+			position--;
+		}
+
 		if (!m_entries[position].isDir)
 			return;
 		
-		if (m_curDir != "/")
-			m_curDir += "/";
 		m_curDir += m_entries[position].name;
+		m_curDir += "/";
 		loadDir();
 	}
 	
 	private void loadDir() {
 		getEntries();
 
-		String[] pp = new String[m_entries.length];
+		m_hasParent = (m_curDir != "/");
+
+		String[] pp = new String[m_entries.length + (m_hasParent ? 1 : 0)];
+		int curPos = 0;
+		if (m_hasParent)
+			pp[curPos++] = "..";
 		for ( int i = 0; i < m_entries.length; i++ ) {
-			pp[i] = m_entries[i].name;
+			pp[curPos++] = m_entries[i].name;
 		}
 		ArrayAdapter<String> adapter = 
 			new ArrayAdapter<String>(this, R.layout.playlist_edit_row, R.id.row_tv, pp);
