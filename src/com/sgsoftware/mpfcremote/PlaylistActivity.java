16410565da065c547058509a77bff12bd8c56ae8
sg2 <sergey.e.galanov@gmail.com>
2011-10-16 14:31:30 +0400

    Fix problems with playlist edit selection.

--- src/com/sgsoftware/mpfcremote/PlaylistActivity.java
+++ src/com/sgsoftware/mpfcremote/PlaylistActivity.java
@@ -1,15 +1,21 @@
 package com.sgsoftware.mpfcremote;
 
+import java.util.BitSet;
+
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.CompoundButton;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.LayoutInflater;
 import android.widget.AdapterView;
+import android.content.Context;
 
 import com.sgsoftware.mpfcremote.RemotePlayer;
 import com.sgsoftware.mpfcremote.MainActivity;
@@ -45,15 +51,11 @@ public class PlaylistActivity extends ListActivity
 			break;
 		}
 		case R.id.playlist_add: {
-			int count = getListView().getChildCount();
-			int i = 0, ei = 0;
-			if (m_hasParent)
-				i++;
-			for ( ; i < count; i++, ei++ ) {
-				CheckBox cb = (CheckBox)((ViewGroup)(getListView().getChildAt(i))).getChildAt(0);
-				boolean ch = cb.isChecked();
-				if (ch)
-					MainActivity.getPlayer().add(m_curDir + "/" + m_entries[ei].name);
+		    BitSet bs = ((MyAdapter)getListAdapter()).getSelected();
+			int count = bs.length();
+			for ( int i = 0; i < count; i++ ) {
+				if (bs.get(i))
+					MainActivity.getPlayer().add(m_curDir + "/" + m_entries[i].name);
 			}
 			break;
 		}
@@ -90,15 +92,7 @@ public class PlaylistActivity extends ListActivity
 
 		m_hasParent = (m_curDir != "/");
 
-		String[] pp = new String[m_entries.length + (m_hasParent ? 1 : 0)];
-		int curPos = 0;
-		if (m_hasParent)
-			pp[curPos++] = "..";
-		for ( int i = 0; i < m_entries.length; i++ ) {
-			pp[curPos++] = m_entries[i].name;
-		}
-		ArrayAdapter<String> adapter = 
-			new ArrayAdapter<String>(this, R.layout.playlist_edit_row, R.id.row_tv, pp);
+		MyAdapter adapter = new MyAdapter(m_hasParent, m_entries);
 		setListAdapter(adapter);
 		
 		((TextView)findViewById(R.id.playlist_curdir)).setText(m_curDir);
@@ -107,6 +101,64 @@ public class PlaylistActivity extends ListActivity
 
 	private void getEntries() {
 		m_entries = MainActivity.getPlayer().listDir(m_curDir);
+	}
+
+	private class MyAdapter extends BaseAdapter
+			implements CompoundButton.OnCheckedChangeListener {
+		private boolean m_hasParent;
+		private RemotePlayer.DirEntry[] m_entries;
+
+		private BitSet m_selected;
+
+		public MyAdapter(boolean hasParent,
+				         RemotePlayer.DirEntry[] entries) {
+			m_hasParent = hasParent;
+			m_entries = entries;
+			m_selected = new BitSet(m_entries.length);
+		}
+
+		public BitSet getSelected() { return m_selected; }
+
+		@Override
+		public int getCount() {
+			return m_entries.length + (m_hasParent ? 1 : 0);
+		}
+
+		@Override
+		public long getItemId(int pos) {
+			return pos;
+		}
+
+		@Override
+		public Object getItem(int pos) {
+			return null;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			ViewGroup vg = (ViewGroup)inflater.inflate(
+					R.layout.playlist_edit_row, parent, false);
+			TextView tv = (TextView)vg.findViewById(R.id.row_tv);
+			CheckBox cb = (CheckBox)vg.findViewById(R.id.row_chbox);
+
+			if (m_hasParent)
+				position--;
+			tv.setText(position == -1 ? ".." : m_entries[position].name);
+
+			if (position >= 0 && m_selected.get(position))
+				cb.setChecked(true);
+			cb.setTag(new Integer(position));
+			cb.setOnCheckedChangeListener(this);
+			return vg;
+		}
+		
+		@Override
+		public void onCheckedChanged(CompoundButton b, boolean isChecked) {
+			int pos = ((Integer)b.getTag()).intValue();
+			if (pos >= 0)
+				m_selected.set(pos, isChecked);
+		}
 	}
 }
 
